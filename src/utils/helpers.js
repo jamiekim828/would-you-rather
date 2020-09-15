e41eea632f45508381db5c25d1a44fa8bd1cbc25
jamiekim828 <jamiekim828@gmail.com>
2020-09-15 09:36:18 +0200

    helpers.js

---
+++ src/utils/helpers.js
@@ -0,0 +1,12 @@
+export function formatQuestion(question, author, authedUser) {
+  const { id, optionOne, optionTwo } = question;
+  const { name, avatarURL } = author;
+
+  return {
+    name,
+    id,
+    avatar: avatarURL,
+    optionOne,
+    optionTwo,
+  };
+}
