ac08647c7bde2c0d5a83683a28c41fd95f8f96d1
jamiekim828 <jamiekim828@gmail.com>
2020-09-24 11:41:11 +0200

    NewQuestion added

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,23 +1,73 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { handleAddQuestion } from '../actions/questions';
 
 class NewQuestion extends Component {
-  state = {
-    option1: '',
-    option2: '',
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      optionOne: '',
+      optionTwo: '',
+      toHome: false,
+    };
+
+    this.handleOptionOne = this.handleOptionOne.bind(this);
+    this.handleOptionTwo = this.handleOptionTwo.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleOptionOne(e) {
+    this.setState({ optionOne: e.target.value });
+  }
+
+  handleOptionTwo(e) {
+    this.setState({ optionTwo: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.props.dispatch(
+      handleAddQuestion(this.state.optionOne, this.state.optionTwo)
+    );
+
+    this.setState(() => ({
+      optionOne: '',
+      optionTwo: '',
+      toHome: true,
+    }));
+  }
 
   render() {
+    if (this.state.toHome === true) {
+      return <Redirect to='/' />;
+    }
     return (
       <div>
         <h2>Create New Question</h2>
         <h4>Complete the question</h4>
         <h3>Would you rather ...</h3>
-        <input />
-        <h4>or</h4>
-        <input />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            placeholder='Write option one here'
+            onChange={this.handleOptionOne}
+            value={this.state.optionOne}
+          />
+          <h4>or</h4>
+          <input
+            type='text'
+            placeholder='Write option two here'
+            onChange={this.handleOptionTwo}
+            value={this.state.optionTwo}
+          />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
 }
 
-export default NewQuestion;
+export default connect(null)(NewQuestion);
