298a90554cac64046db76a56f4d69da2677f43e6
jamiekim828 <jamiekim828@gmail.com>
2020-09-25 12:37:59 +0200

    LeaderBoard working without authedUser

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -3,16 +3,37 @@ import { connect } from 'react-redux';
 
 class LeaderBoard extends Component {
   render() {
-    const { users } = this.props;
+    const users = this.props.usersList;
+    console.log(users);
 
-    return <div></div>;
+    return (
+      <div>
+        {users.map((user) => (
+          <div>
+            <div>
+              <img alt={`${user.id} avatar`} src={user.avatarURL} />
+            </div>
+            <div>
+              <h2>{user.name}</h2>
+              <h4>Answered questions : {Object.keys(user.answers).length}</h4>
+              <h4>Created questions : {user.questions.length}</h4>
+            </div>
+            <div>
+              <h4>Score</h4>
+              <p>{Object.keys(user.answers).length + user.questions.length}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
 function mapStateToProps(state) {
-  console.log('leader', state);
   const users = state.users;
-  return { users };
+  const usersList = Object.entries(users).map((e) => e[1]);
+
+  return { usersList };
 }
 
 export default connect(mapStateToProps)(LeaderBoard);
